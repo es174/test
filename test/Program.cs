6c3b44ae0622179b03d6b6d12c08136dba14c18e
es174 <egor74mag@gmail.com>
2022-04-05 00:27:22 +0500

    Добавьте файлы проекта.

---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 39, 78, 56, 36, 24, 25, 15, 15, 89, 71 };
+            int i, j, t;
+            for (i = 0; i < a.Length - 1; i++)
+                for (j = i + 1; j < a.Length; j++)
+                    if (a[i] > a[j])
+                    {
+                        t = a[i];
+                        a[i] = a[j];
+                        a[j] = t;
+                    }
+            for (i = 0; i < a.Length; i++)
+                Console.Write(a[i] + " ");
+        }
+    }
+}
